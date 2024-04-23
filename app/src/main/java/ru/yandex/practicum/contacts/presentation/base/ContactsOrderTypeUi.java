a4b4797b93ab68ab2a96e28b66af3a5e132ee17e
Vitaliy Yarash <vit.iarosch@yandex.by>
2024-04-23 08:33:07 +0300

    feat: add 2 task

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ContactsOrderTypeUi.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ContactsOrderTypeUi.java
@@ -2,20 +2,18 @@ package ru.yandex.practicum.contacts.presentation.base;
 
 import androidx.annotation.NonNull;
 
-public class ContactsOrderTypeUi {
-    private final String type;
-    private final boolean isSelected;
-
+abstract public class ContactsOrderTypeUi {
+    protected final String type;
+    protected final boolean isSelected;
     public ContactsOrderTypeUi(@NonNull String type, boolean isSelected) {
         this.type = type;
         this.isSelected = isSelected;
     }
-
     public String getType() {
         return type;
     }
-
     public boolean isSelected() {
         return isSelected;
     }
+    public abstract String createLogMessage();
 }
